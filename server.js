208e35ab5d9719d93c906d6c8a7d6187468a3c60
Pablo Plata <pplata1631@gmail.com>
2018-05-21 00:41:08 -0500

    packages

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var bodyparser = require('body-parser');
+var methodOverride = require('method-override');
+var exphbs = require('express-handlebars');
+
+var app = express();
+app.use(express.static(_dirname + '/public'));
+
+app.use(bodyParser.urlencoded({
+    extended: flase
+}))
+
+app.use(methodOverride('_method'));
+app.engine('handlerbars',exphbs({
+    defaultLayout:'main'
+}));
+app.set('view engine','handlerbars');
